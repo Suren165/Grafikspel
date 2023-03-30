5e3282472a5efa492e783a3af05a47021a320be4
Suren Arakelyan <suren.arakelyan@elev.ga.ntig.se>
2023-03-30 15:17:57 +0200

    HELLO

--- Grafikspel/Program.cs
+++ Grafikspel/Program.cs
@@ -12,9 +12,9 @@ Texture2D Imageenemy = Raylib.LoadTexture("monster.png");
 Texture2D avatarImage = Raylib.LoadTexture("avatar.png");
 
 
-Rectangle character = new Rectangle(0, 60, avatarImage.width, avatarImage.height);
+Rectangle character = new Rectangle(0, 60, avatarImage.width, avatarImage.height); // Main gubben
 
-Rectangle enemyrect = new Rectangle(700, 500, 64, 64);
+Rectangle enemyrect = new Rectangle(700, 500, 64, 64); // onda gubben som följer efter dig 
 
 Color mycolor = new Color(47, 79, 79, 255);
 
@@ -25,15 +25,15 @@ float enemySpeed = 1;
 
 void tid()
 {
-    enemySpeed = enemySpeed + 0.1f;
+    enemySpeed = enemySpeed + 0.1f;   // Onda gubbe blir snabbare under varje sekund som man överlever  
 }
-System.Timers.Timer timer = new(interval: 1000);
+System.Timers.Timer timer = new(interval: 1000); // i ett interval under 1000 sekunder 
 timer.Elapsed += (sender, e) => tid();
 
-int points = 0;
-int winScore = 8000;
+int points = 0; // börjar på 0
+int winScore = 8000; // du vinner när man når 8000 poäng
 
-void reset()
+void reset() // när spelet börjar om så ska det här vara standard som man börjar på
 {
     enemyrect.x = 700;
     enemyrect.y = 500;
@@ -43,11 +43,14 @@ void reset()
     enemySpeed = 1;
 }
 
-while (Raylib.WindowShouldClose() == false)
+while (true) //While loop
 {
+   if (Raylib.WindowShouldClose())
+   {
+    break;  
+   } //kontrollera om ett fönster i Raylib-biblioteket ska stängas.
 
-
-    if (currentscene == "game")
+    if (currentscene == "game") // när man spelar så gäller dessa knappar  A W S
     {
         timer.Start();
         Console.WriteLine(enemySpeed);
@@ -73,13 +76,13 @@ while (Raylib.WindowShouldClose() == false)
         }
         if (Raylib.CheckCollisionRecs(character, enemyrect))
         {
-            currentscene = "gameover";
+            currentscene = "gameover";// om man förlorar så stannar timer 
             timer.Stop();
         }
 
-        Raylib.DrawText("Points:" + points,20,20,24, Color.RED);
-          
-        
+        Raylib.DrawText("Points:" + points, 20, 20, 24, Color.RED); // röd färg på poäng
+
+
 
 
         Vector2 playerpos = new Vector2(character.x, character.y);
@@ -93,13 +96,13 @@ while (Raylib.WindowShouldClose() == false)
         enemyMovement = enemyDirection * enemySpeed;
 
         enemyrect.x += enemyMovement.X;
-        enemyrect.y += enemyMovement.Y;
+        enemyrect.y += enemyMovement.Y;          // allt dessa förklarar hur onda gubben följer efter main gubben
 
     }
-    else if (currentscene == "start")
+    else if (currentscene == "start") 
     {
 
-        if (Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))  // om man trycker på enter på första skärmen så får man upp spelet upp på sin skärm.
         {
             currentscene = "game";
 
@@ -109,7 +112,7 @@ while (Raylib.WindowShouldClose() == false)
     Raylib.BeginDrawing();
 
     Raylib.ClearBackground(Color.WHITE);
-    
+
     if (currentscene == "game")
     {
         Raylib.DrawTexture(avatarImage,
@@ -119,40 +122,42 @@ while (Raylib.WindowShouldClose() == false)
         );
 
         Raylib.DrawTexture(Imageenemy, (int)enemyrect.x, (int)enemyrect.y, Color.WHITE);
-           
-        if (points >= winScore){
+
+        if (points >= winScore)
+        {
             currentscene = "win";
-        }
+        }                                              //om man vinner så får man upp win
 
-        
+
     }
-
+                                                
     else if (currentscene == "start")
     {
-        
-        Raylib.DrawText("Press Enter to start", 100, 300, 70, Color.BLACK);
-        Raylib.DrawText(" W= upp S=ner D=höger A= vänster", 100, 500, 20, Color.BLACK);
 
-    }
-     else if (currentscene == "win")
+        Raylib.DrawText("Press Enter to start", 100, 300, 70, Color.BLACK);
+        Raylib.DrawText("Instruktioner: Du ska överleva så länge du kan untan att nudda din fiende men tänk på att din fiende blir snabbare.", 40, 500, 16, Color.BLACK);
+        Raylib.DrawText(" W= upp S=ner D=höger A= vänster", 100, 550, 20, Color.BLACK);
+        
+    }       // grafik
+    else if (currentscene == "win")
     {
         Raylib.DrawText("You Win", 100, 500, 128, Color.BLACK);
     }
     else if (currentscene == "gameover")
     {
-        Raylib.DrawText("GAME OVER", 100, 500, 128, Color.BLACK);
+        Raylib.DrawText("GAME OVER", 100, 500, 128, Color.BLACK);                  // om du blir fångad av onda gubben så får du upp game over.
 
-         reset();
-        if(Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
+        reset();
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
         {
             currentscene = "start";
         }
     }
-   
 
 
 
-    
+
+
 
 
 
